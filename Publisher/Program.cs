89c71b5c696eceac5a60a8f1c0278c4048fed0c6
Neil Ramsbottom <nramsbottom@hotmail.com>
2019-10-10 15:40:56 +0200

    Initial version. A publisher and subscriber console application.

---
+++ Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using MassTransit;
+using System;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(x =>
+                    x.Host(new Uri("rabbitmq://localhost/"), h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    }));
+
+            bus.Start();
+
+            while (true)
+            {
+
+                bus.Publish<Contracts.SomethingHappened>(new Messages.SomethingHappened()
+                {
+                    What = "A message was sent.",
+                    When = DateTime.Now
+                });
+
+                Console.WriteLine("Message sent. Press any key to send another.");
+                Console.ReadKey();
+            }
+
+            bus.Stop();
+        }
+    }
+}
+
+namespace Publisher.Messages
+{
+    public class SomethingHappened : Contracts.SomethingHappened
+    {
+        public string What { get; set; }
+
+        public DateTime When { get; set; }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,54 @@
+﻿using Contracts;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(x =>
+            {
+
+                var host = x.Host(new Uri("rabbitmq://localhost/"), config =>
+                {
+                    config.Username("guest");
+                    config.Password("guest");
+                });
+
+                x.ReceiveEndpoint(host, "QueueTestQueue", e =>
+                {
+                    e.Consumer<Subscriber.Consumers.SomethingHappenedConsumer>();
+                });
+            });
+
+            bus.Start();
+            Console.ReadKey();
+            bus.Stop();
+        }
+    }
+}
+
+namespace Subscriber.Consumers
+{
+    public class SomethingHappenedConsumer : IConsumer<Contracts.SomethingHappened>
+    {
+        public Task Consume(ConsumeContext<SomethingHappened> context)
+        {
+            Console.WriteLine("Consumed the message.");
+            return Task.FromResult(0);
+        }
+    }
+}
+
+namespace Subscriber.Messages
+{
+    public class SomethingHappened : Contracts.SomethingHappened
+    {
+        public string What { get; set; }
+
+        public DateTime When { get; set; }
+    }
+}
