2348c1e009b74ad975be73212fbaddc114cf01e2
Neil Ramsbottom <nramsbottom@hotmail.com>
2019-10-10 15:41:25 +0200

    Added an ASP.NET MVC site that should be able to publish, but for some reason the messages aren't going into the queue.

---
+++ PublishingWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PublishingWebApplication.Models;
+using System;
+using System.Diagnostics;
+
+namespace PublishingWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm] string action)
+        {
+
+            PublishMessage();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        private void PublishMessage()
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(x =>
+                               x.Host(new Uri("rabbitmq://localhost/"), h =>
+                               {
+                                   h.Username("guest");
+                                   h.Password("guest");
+                               }));
+
+            bus.Start();
+
+            bus.Publish<Contracts.SomethingHappened>(new Messages.SomethingHappened()
+            {
+                What = "A message was sent from ASP.NET",
+                When = DateTime.Now
+            });
+
+            bus.Stop();
+        }
+
+    }
+}
---
+++ PublishingWebApplication/Messages/SomethingHappened.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublishingWebApplication.Messages
+{
+    public class SomethingHappened : Contracts.SomethingHappened
+    {
+        public string What { get; set; }
+
+        public DateTime When { get; set; }
+    }
+}
